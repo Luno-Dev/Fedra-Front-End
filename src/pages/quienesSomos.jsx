2dc265043dea0a6febae8ac074a30bc9ff8548ae
Franco Osman <francoosman12@gmail.com>
2023-06-05 00:01:43 -0300

    Comienzo de -Quienes somos-

---
+++ src/pages/quienesSomos.jsx
@@ -0,0 +1,14 @@
+import Navs from "@/components/common/Navs";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const quienesSomos = () => {
+    return (
+        <>
+            <Navs></Navs>
+            <h1>hola</h1>
+        </>
+    );
+};
+
+export default quienesSomos;
