4181b1af7be776b78b85bf3f2154abd1621ad943
Gabriel Azubel <gabrielazubel63t@gmail.com>
2023-06-26 11:41:57 -0300

    redireccion de iconos en footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -21,29 +21,34 @@ const Footer = () => {
               >
                 <h6>Seguinos en</h6>
                 <div className="d-flex justify-content-between">
-                  <Nav.Link href="#">
+                  <Nav.Link href="https://www.instagram.com/fedraargentina/" target="_blank">
                     <i className="bi bi-instagram text-cyan mx-2"></i>
                   </Nav.Link>
-                  <Nav.Link href="#">
+                  <Nav.Link href="https://twitter.com/FedraArgentina" target="_blank">
                     <i className="bi bi-twitter text-cyan mx-2"></i>
                   </Nav.Link>
-                  <Nav.Link href="#">
+                  <Nav.Link href="https://www.facebook.com/profile.php?id=100093740438104&mibextid=ZbWKwL" target="_blank">
                     <i className="bi bi-facebook text-cyan mx-2"></i>
                   </Nav.Link>
                 </div>
               </Col>
               <Col
                 md={4}
-                className="d-flex flex-column align-items-center justify-content-center">
+                className="d-flex flex-column align-items-center justify-content-center"
+              >
                 <h6>Contacto</h6>
-                <div className="d-flex">
-                  <i className="bi bi-telephone text-cyan me-2"> </i>
-                  <p>555-555 555</p>
-                </div>
-                <div className="d-flex">
-                  <i className="bi bi-envelope-at text-cyan me-2"> </i>
-                  <p>fedra@gmail.com</p>
-                </div>
+                  <a href="https://wa.me/+5493816339323" target="_blank" className="d-flex text-decoration-none">
+                    <i className="bi bi bi-whatsapp text-cyan me-2"></i>
+                    <p>+5493816339323</p>
+                  </a>
+                <a href="tel:+543812326414" className="d-flex text-decoration-none">
+                  <i className="bi bi-telephone text-cyan me-2"></i>
+                  <p>+543812326414</p>
+                </a>
+                <a href="mailto:fedraArgentina@gmail.com" className="d-flex text-decoration-none">
+                  <i className="bi bi-envelope-at text-cyan me-2"></i>
+                  <p>fedraArgentina@gmail.com</p>
+                </a>
               </Col>
             </Row>
           </Container>
