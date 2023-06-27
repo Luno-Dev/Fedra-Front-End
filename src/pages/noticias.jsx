2d7b8605944deb5824e8c205fb71b9fc245227b3
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-06-27 16:41:34 -0300

    se mejora

--- src/pages/noticias.jsx
+++ src/pages/noticias.jsx
@@ -1,14 +1,14 @@
 import Navs from '@/components/common/Navs'
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { traerNoticias  } from '@/helpers/fetchAdmi';
+import { traerNoticias } from '@/helpers/fetchAdmi';
 import { useContext, useEffect, useState } from 'react';
-import  DataContext  from '../components/context/DataContext';
+import DataContext from '../components/context/DataContext';
 
 const noticias = () => {
   const [noticias, setNoticias] = useState([]);
 
- 
+
 
 
 
@@ -17,59 +17,59 @@ const noticias = () => {
 
     const noticias = await traerNoticias();
     setNoticias(noticias.noticias);
-    
+
   }
   useEffect(() => {
     recibirData();
-  
-    
+
+
   }, [])
-  
+
   return (
     <>
 
-    <div>
+      <div>
 
-       <Helmet>
-        <meta charSet="utf-8" />
-        {/*     <link rel="shortcut icon" href={favicon} type="image/x-icon" /> */}
-        <title>Noticias</title>
-      </Helmet>
-    </div>
-     
-    <Navs/>
-    <div className='Contenedor-noticias'>
-      <h1 className='text-center'>Noticias</h1>
-      <div className="not-cont">
-        
-        
-    {noticias.map(index => (
-        <div key={index._id}  className="noticias-card">
-        <div className="card-img-noticia">
-          { index.img.length > 1 ?
-            <img src={index.img} alt="img" />: <span> </span> }
-            </div>
-          <div className="card-body-noticia">
-            <h3 className='noticias-titulo'>{index.titulo}</h3>
-            <p className='noticias-descripcion'>{index.descripcion}</p>
-            
-            <p className='noticias-autor'>{index.autor}</p>
-            <p className='noticias-fecha'>{index.fecha.split("T",1)}</p>
-            
-          </div>
-        </div>
-        ))}
-        
-       
-     
-      
-        
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Noticias</title>
+        </Helmet>
       </div>
 
-    </div>
-    
+      <Navs />
+      <div className='Contenedor-noticias'>
+        <h1 className='text-center'>Noticias</h1>
+        <div className="not-cont">
+
+
+          {noticias.map(index => (
+            <div key={index._id} className="noticias-card ">
+
+              {index.img < 1 ?
+                <div className="card-img-noticia">
+                  <img src={index.img} alt="img" /></div> : <span></span>}
+
+              <div className="card-body-noticia">
+                <h3 className='noticias-titulo mt-5'>{index.titulo}</h3>
+                <p className='noticias-descripcion'>{index.descripcion}</p>
+
+                <p className='noticias-autor'>{index.autor}</p>
+                <p className='noticias-fecha'>{index.fecha.split("T", 1)}</p>
+
+              </div>
+            </div>
+          ))}
+
+
+
+
+
+        </div>
+
+      </div>
+
     </>
-    
+
   )
 }
 
